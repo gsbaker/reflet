4e2e36ff07e4dfd08d0ba9421785bbf22f54a641
George Baker <36203880+gsbaker@users.noreply.github.com>
2024-04-20 15:31:36 +0100

    Add cards and color coded needs to dashboard

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,11 @@
 module ReportsHelper
+  def need_color_class(rating)
+    color_classes = {
+      "Rarely Met" => "need--rare",
+      "Sometimes Met" => "need--sometimes",
+      "Consistently Met" => "need--consistent"
+    }
+
+    color_classes[rating.title]
+  end
 end
