94a3871300c4e09b52fba4fc3b6e51de6a17385b
George Baker <36203880+gsbaker@users.noreply.github.com>
2024-04-20 15:17:24 +0100

    Add report summaries

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,5 +1,5 @@
 class RatingsController < ApplicationController
-  before_action :set_rating, only: %i[ show edit update destroy ]
+  before_action :set_rating, only: %i[update]
 
   # PATCH/PUT /ratings/1 or /ratings/1.json
   def update
