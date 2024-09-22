e95efcbca5e5ce3adc43e414c1e631cd1128fe4a
George Baker <36203880+gsbaker@users.noreply.github.com>
2024-09-22 10:54:14 +0100

    Add PHQ9 questionnaire

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,24 +16,38 @@ needs.each do |need|
   Need.find_or_create_by!(title: need["title"], category: need["category"])
 end
 
-gad7_questionnaire = Questionnaire.find_or_create_by!(
-  title: "GAD-7",
-  description: "Over the last two weeks, how often have you been bothered by any of the following problems?"
-)
-
-gad7_questions = YAML.load_file("db/seeds/gad7.yml")
-
-gad7_response_options = {
+response_options = {
   0 => "Not at all",
   1 => "Several days",
   2 => "More than half the days",
   3 => "Nearly every day"
 }
 
+gad7_questionnaire = Questionnaire.find_or_create_by!(
+  title: "Generalised Anxiety Disorder Questionnaire (GAD-7)",
+  description: "Over the last two weeks, how often have you been bothered by any of the following problems?"
+)
+
+gad7_questions = YAML.load_file("db/seeds/gad7.yml")
+
 gad7_questions.each do |question|
   gad7_questionnaire.questions.find_or_create_by!(
     title: question["title"],
-    response_options: gad7_response_options
+    response_options:
+  )
+end
+
+phq9_questionnaire = Questionnaire.find_or_create_by!(
+  title: "Patient Health Questionnaire (PHQ-9)",
+  description: "Over the last two weeks, how often have you been bothered by any of the following problems?"
+)
+
+phq9_questions = YAML.load_file("db/seeds/phq9.yml")
+
+phq9_questions.each do |question|
+  phq9_questionnaire.questions.find_or_create_by!(
+    title: question["title"],
+    response_options:
   )
 end
 
