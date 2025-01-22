d26fc2724f6b2eaffc6ae2420f43d3a2dd3c6300
George Baker <36203880+gsbaker@users.noreply.github.com>
2025-01-22 15:11:46 +0000

    Create assignments from therapy page

--- app/controllers/therapies/assignments_controller.rb
+++ app/controllers/therapies/assignments_controller.rb
@@ -2,6 +2,24 @@ module Therapies
   class AssignmentsController < BaseController
     def index
       @assignments = @therapy.assignments
+      @assignment = @therapy.assignments.build
+    end
+
+    def create
+      @assignment = @therapy.assignments.build(assignment_params)
+      @assignment.assignable_type = "Questionnaire"
+
+      if @assignment.save!
+       redirect_to therapy_assignments_path(@therapy)
+      else
+        render :index, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def assignment_params
+      params.require(:assignment).permit(:assignable_id, :cadence)
     end
   end
 end
